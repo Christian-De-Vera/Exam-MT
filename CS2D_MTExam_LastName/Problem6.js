9a2bfa977ef9ead9b5bbde6ee88db48a1408c406
Christian <zen.chishiki404@gmail.com>
2024-10-30 21:53:13 +0800

    update

---
+++ CS2D_MTExam_LastName/Problem6.js
@@ -0,0 +1,9 @@
+// stores the user profile, I use prompt cuz I just feel like doing it
+let userProfile = {
+    fullName:prompt(`Enter your name: `),
+    age:prompt(`Enter your age: `),
+    favNumber:prompt(`Enter your Favourite number: `),
+    favColor:prompt(`Enter your favourite color: `)
+}
+
+console.log(userProfile);
