f460437b7a8f31f137ed2a4b94ca06a0511d6884
Christian <zen.chishiki404@gmail.com>
2024-10-30 20:34:25 +0800

    Update Problem5.js

---
+++ CS2D_MTExam_LastName/Problem5.js
@@ -0,0 +1,11 @@
+let userlikes = []; //empty array
+
+//The following just prompt the user and directly add it in the array
+userlikes.push(prompt(`First color that you like: `))
+console.log(userlikes);
+
+userlikes.push(prompt(`Second color that you like: `))
+console.log(userlikes);
+
+userlikes.push(prompt(`Third color that you like: `))
+console.log(userlikes);
