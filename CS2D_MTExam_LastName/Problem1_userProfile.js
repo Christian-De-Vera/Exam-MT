4ae49a0440eab0241d3bff342736ac3c5500485f
Christian <zen.chishiki404@gmail.com>
2024-10-30 17:52:03 +0800

    Create Problem1_userProfile.js

---
+++ CS2D_MTExam_LastName/Problem1_userProfile.js
@@ -0,0 +1,12 @@
+//User Information Collection
+
+let fullName = prompt("Your full name: ");
+let age = prompt("Your age: ");
+let favNumber = prompt("Your Favourite Number: ");
+let favColor = prompt("Your favourite color: ");
+
+//logs it to the console
+console.log(fullName);
+console.log(age);
+console.log(favNumber);
+console.log(favColor);
